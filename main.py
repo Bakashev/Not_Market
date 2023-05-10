1daf391d2e7ba6343dcaa9fd82f43abbbfcebda1
bakashev <bakashevs@mail.ru>
2023-05-10 18:18:49 +0300

    Correct main file

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import menu
+import connect_db
+from models import Base
+import models.users
+import models.tickets
+import models.orders
+import models.products
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+
 if __name__ == '__main__':
     print('PyCharm')
+
+connect_db.configurate_engine()
+Base.metadata.drop_all(bind=connect_db.engine)
+Base.metadata.create_all(bind=connect_db.engine)
 
 while True:
     start = menu.StartMenu()
